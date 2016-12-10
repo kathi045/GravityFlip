4c9d245a1b956ae46c3c52cf2fb8a73004452909
kathi045 <kathi045@gmail.com>
2016-12-10 01:43:50 +0100

    also added new files

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	void Update () {
+		Vector3 scale = transform.localScale;
+		scale.x = 0.0035f;
+		transform.localScale = scale;
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+	[SyncVar] public int currentHealth = maxHealth;
+	public RectTransform healthbar;
+
+	public void TakeDamage(int amount) {
+		if (!isServer) {
+			return;
+		}
+
+		currentHealth -= amount;
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log ("Dead!");
+		}
+
+		healthbar.sizeDelta = new Vector2 (currentHealth, healthbar.sizeDelta.y);
+	}
+}
