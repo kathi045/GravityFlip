164c1639baa49b0f0a1ba2e394e7b28d4e806df7
kathi045 <kathi045@gmail.com>
2016-12-11 12:58:45 +0100

    continued networking

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : NetworkBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col) {
-		GameObject hit = col.gameObject;
+		GameObject hit = col.gameObject;			// gameobject that was hit by the bullet
 		Health health = hit.GetComponent<Health> ();
 
 		if (health != null) {
