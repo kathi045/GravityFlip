100d08dbcf987ef491aeffeb8c1e72c563e67141
Kathi <kathi045@gmail.com>
2017-01-12 14:13:08 +0100

    Player speed now resetting after death

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameController : NetworkBehaviour {
+
+    private static float gravity = 2.0f;                // default gravity
+    private static float defaultPlayerSpeed = 6.0f;     // default player walk speed
+    private static float maxPlayerSpeed = 12.0f;        // default maximum player walk speed
+    private static float bulletSpeed = 6.0f;            // default bullet speed
+    private static int bulletDamage = 15;               // default damage of a single bullet
+    private static int maximumPlayerHealth = 100;       // default player health
+    private bool dieOnPlayerCollision = true;           // default for chase mode
+
+    public static Text scoreTextPlayer1;
+    public static Text scoreTextPlayer2;
+    public static int scorePlayer1;
+    public static int scorePlayer2;
+
+	// Use this for initialization
+	void Start () {
+        scorePlayer1 = 0;
+        scorePlayer2 = 0;
+        scoreTextPlayer1 = GameObject.Find("ScorePlayer1").GetComponent<Text>();
+        scoreTextPlayer2 = GameObject.Find("ScorePlayer2").GetComponent<Text>();
+        UpdateScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	[Command]
+	public void CmdUpdateScore() {
+		scoreTextPlayer1.text = "You:   " + scorePlayer1;
+		scoreTextPlayer2.text = "Enemy:   " + scorePlayer2;
+	}
+
+    public static void UpdateScore()
+    {
+        scoreTextPlayer1.text = "You:   " + scorePlayer1;
+        scoreTextPlayer2.text = "Enemy:   " + scorePlayer2;
+    }
+
+    public static void AddScore(int newScoreValuePlayer1, int newScoreValuePlayer2)
+    {
+        scorePlayer1 += newScoreValuePlayer1;
+        scorePlayer2 += newScoreValuePlayer2;
+        UpdateScore();
+    }
+
+    public static float GetGravity()
+    {
+        return gravity;
+    }
+
+    public static float GetMaximumPlayerSpeed()
+    {
+        return maxPlayerSpeed;
+    }
+
+    public static float GetDefaultPlayerSpeed()
+    {
+        return defaultPlayerSpeed;
+    }
+
+    public static float GetBulletSpeed()
+    {
+        return bulletSpeed;
+    }
+
+    public static int GetBulletDamage()
+    {
+        return bulletDamage;
+    }
+
+    public static int GetMaximumPlayerHealth()
+    {
+        return maximumPlayerHealth;
+    }
+
+    public bool GetDieOnPlayerCollision()
+    {
+        return dieOnPlayerCollision;
+    }
+
+    public void SetDieOnPlayerCollision(bool chaseModeActivated)
+    {
+		dieOnPlayerCollision = chaseModeActivated;
+    }
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,19 +6,24 @@ using UnityEngine.Networking;
 
 public class Health : NetworkBehaviour {
 
-	public const int maxHealth = 100;
+    public static int maxHealth = GameController.GetMaximumPlayerHealth();
+
 	[SyncVar (hook = "OnChangeHealth")] public int currentHealth = maxHealth;
 	public RectTransform healthbar;
 
-	public void TakeDamage(int amount) {
+	public void TakeDamage(int amount, bool victory) {
 		if (!isServer) {
 			return;
 		}
-
+        
 		currentHealth -= amount;
 		if (currentHealth <= 0) {
 			currentHealth = maxHealth;
-			RpcRespawn();
+            GetComponentInParent<PlayerController>().RpcIncreaseSpeed(0.5f);
+			if (victory == true) {
+				RpcAddScore ();
+			}
+            RpcRespawn();
 		}
 	}
 
@@ -30,7 +35,25 @@ public class Health : NetworkBehaviour {
 	[ClientRpc]
 	void RpcRespawn() {
 		if (isLocalPlayer) {
-			transform.position = Vector3.zero;
+			Vector3 newSpawnPosition = Vector3.zero;
+            GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+            int spawnIndex = Random.Range(0, platforms.Length);
+            newSpawnPosition = platforms[spawnIndex].GetComponent<Transform>().position;
+            //newSpawnPosition.y -= 1;
+            transform.position = newSpawnPosition;
+			GetComponentInParent<PlayerController> ().ResetPlayerSpeed ();
+            //GameObject.Find("DebugMessage").GetComponent<Text>().text = "Debug: Position: " + transform.position + " | PlatformIndex: " + spawnIndex + " | Platform length: " + platforms.Length;
 		}
 	}
+
+    [ClientRpc]
+	void RpcAddScore()
+    {
+		if (isLocalPlayer) {
+			GameController.AddScore (0, 100);
+		} else {
+			GameController.AddScore (100, 0);
+		}
+
+    }
 }
