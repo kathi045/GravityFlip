aedefff046b62114db7b9f722175d93abea3ac8f
Roman Habitzl <Roman.Habitzl@gmx.net>
2017-01-12 19:00:10 +0100

    added custom target cursor, fixed bullets spawning in wall

---
+++ Assets/Scripts/CursorSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSettings : MonoBehaviour {
+
+    public Texture2D targetCursorTexture;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.SetCursor(targetCursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+	
+	void Awake ()
+    {
+        Cursor.SetCursor(targetCursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+}
